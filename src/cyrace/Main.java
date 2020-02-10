158140103c2a62464a992e743aedab5d027e495d
sujoypodder <sujoy.podder1995@gmail.com>
2020-02-11 02:00:10 +0600

    first commit

---
+++ src/cyrace/Main.java
@@ -0,0 +1,19 @@
+package cyrace;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+public class Main extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		Pop.pop.render((Graphics2D) g);
+	}
+
+}
